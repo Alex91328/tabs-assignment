f579fd07b4ac606d118b60dd17ee062b16805ba7
Alex91328 <57780963+Alex91328@users.noreply.github.com>
2020-03-10 10:11:33 -0600

    Second Commit of this Assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Tabs from './components/Tabs';
+import Content from './components/Content';
+
 
 function App() {
+
+  const [setIndex, setIndexState] = useState(0)
+
+  const changeValue = (textIndex) => {
+    setIndexState(textIndex)
+  }
+
+  const textArray = [
+    "cats are beast yo",
+    "yo mama",
+    "This is the third tab now"
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs changeValue={() => changeValue(0)} tabLabel="Tab 1" />
+      <Tabs changeValue={() => changeValue(1)} tabLabel="Tab 2" />
+      <Tabs changeValue={() => changeValue(2)} tabLabel="Tab 3" />
+      <p><Content displayText={textArray[setIndex]} /></p>
     </div>
   );
 }
---
+++ src/components/Tabs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+const Tabs = (props) => {
+    return (
+        <button onClick={props.changeValue}>{props.tabLabel}</button>
+    )
+}
+
+export default Tabs;
